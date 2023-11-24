3ac963e9135cc1a6c7036aab1f087fd28465fe56
ReverendV92 <ReverendV92@users.noreply.github.com>
2023-11-24 17:39:49 -0600

    Update util_buddyfinder.lua

--- .content/buddy_finder-redux/lua/weapons/util_buddyfinder.lua
+++ .content/buddy_finder-redux/lua/weapons/util_buddyfinder.lua
@@ -242,7 +242,7 @@ if CLIENT then
 	function SWEP:DoFinishCall()
 		--stop all sounds if they are still playing
 		for k,v in pairs(self.SoundObjects) do
-		v:Stop()
+			v:Stop()
 		end
 	end
 
@@ -264,12 +264,12 @@ if CLIENT then
 		btnSend:SetText("Send")
 
 		local function SendPMToServer()
-		local txtSend = teMsg:GetValue()
-		if (txtSend != '') then		
-			LocalPlayer():ConCommand(BuddyFinderCC.PM.." "..playerSteamId.."\""..txtSend.."\"")
-			self:MakePMSound()
-			frPM:Remove()
-		end
+			local txtSend = teMsg:GetValue()
+			if (txtSend != '') then		
+				LocalPlayer():ConCommand(BuddyFinderCC.PM.." "..playerSteamId.."\""..txtSend.."\"")
+				self:MakePMSound()
+				frPM:Remove()
+			end
 		end
 
 		teMsg.OnEnter = function() SendPMToServer() end
@@ -278,10 +278,12 @@ if CLIENT then
 		frPM:MakePopup()
 		teMsg:RequestFocus()
 		frPM:DoModal()
+
 	end
 
 	--the contact list menu
 	function SWEP:ShowPhonePlayerList(requestingTeleport)
+
 		local frDirectory = vgui.Create("DFrame")
 		frDirectory:SetSize(500, 500)
 		frDirectory:SetTitle("Buddy Finder - Contacts")
@@ -353,9 +355,9 @@ end
 
 if SERVER then
 
-	SWEP.Weight =	0
-	SWEP.AutoSwichTo =	false
-	SWEP.AutoSwichFrom =	false
+	SWEP.Weight = 0
+	SWEP.AutoSwichTo = false
+	SWEP.AutoSwichFrom = false
 
 	--if the owner is an NPC then we call an alternate routine as
 	--the NPC doesn't have a clientside
@@ -401,12 +403,12 @@ if SERVER then
 		timer.Simple(4, function() tunnel:Remove() end)
 	end
 
-	--creates/plays and automatically cleans up... the tunnel effects
+	-- creates/plays and automatically cleans up... the tunnel effects
 	function SWEP:TunnelEffect(	ply	)
 		local tunnel = ents.Create('prop_dynamic')
-		//tunnel:SetModel('models/props_combine/stasisvortex.mdl')
+		-- tunnel:SetModel('models/props_combine/stasisvortex.mdl')
 		tunnel:SetModel('models/effects/vol_light128x128.mdl')
-		--tunnel:SetPos(	ply:GetPos() + tunnel:BoundingRadius() * Vector(0, 0, 1))
+		-- tunnel:SetPos(	ply:GetPos() + tunnel:BoundingRadius() * Vector(0, 0, 1))
 		tunnel:SetPos(	ply:GetPos() + tunnel:BoundingRadius() * Vector(0, 0, 128))
 		tunnel:SetOwner(	ply	)
 		tunnel:DrawShadow(false)
@@ -415,7 +417,7 @@ if SERVER then
 		self:DelayedTunnelRemove(tunnel)
 	end
 
-	--jump to the destination, triggering handlers as we go
+	-- jump to the destination, triggering handlers as we go
 	function SWEP:DoTeleport(destination)
 		local ply = self:GetOwner()
 		self:DoPreTeleport()
@@ -426,16 +428,16 @@ if SERVER then
 		self:DoPostTeleport()
 	end
 
-	--tell the clientside to start pre-teleport sound and video effects
-	--this also spawns a self-managing tunnel effect
+	-- tell the clientside to start pre-teleport sound and video effects
+	-- this also spawns a self-managing tunnel effect
 	function SWEP:DoPreTeleport()
 		local ply = self:GetOwner()
 		self:TunnelEffect(	ply	)
 		self:SafeCallOnClient("DoPreTeleport", "")
 	end
 
-	--tell the clientside to start post-teleport sound and video effects
-	--this also spawns a self-managing tunnel effect
+	-- tell the clientside to start post-teleport sound and video effects
+	-- this also spawns a self-managing tunnel effect
 	function SWEP:DoPostTeleport()
 		local ply = self:GetOwner()
 		self:TunnelEffect(	ply	)
@@ -443,25 +445,25 @@ if SERVER then
 		self:BuddyFinderBusy(false)
 	end
 
-	--tell the clientside to start warmup sound and video effects
+	-- tell the clientside to start warmup sound and video effects
 	function SWEP:DoTeleportWarmup()
 		self:SafeCallOnClient("DoTeleportWarmup", "")
 	end
 
-	--send the "rang out" message back to the player calling
+	-- send the "rang out" message back to the player calling
 	function SWEP:BuddyRangOut()
 		self:NotifyIncomingCallHUD(false)
 		self:SendBuddyCallInfo(BuddyCallInfo.bciBusy, self.Caller)
 		self:FinishCall()
 	end
 
-	--enable or disable the incoming call blinking HUD icon
+	-- enable or disable the incoming call blinking HUD icon
 	function SWEP:NotifyIncomingCallHUD(enable)
 		self:SendHUDNotification(enable, BuddyCallInfo.bciIncoming)
 	end
 
-	--spawn the laser box and allow it to be moved about by the 
-	--direction the user is facing
+	-- spawn the laser box and allow it to be moved about by the 
+	-- direction the user is facing
 	function SWEP:StartSelectPoint()
 		self:BuddyAnim(BuddyFinderAnim.LaserBoxIdle, false)
 		if (self.LaserBox == nil) then
@@ -477,15 +479,15 @@ if SERVER then
 		end
 	end
 
-	--return true if the buddy finder is on a call
+	-- return true if the buddy finder is on a call
 	function SWEP:HasIncomingCall()
 		return (self.Caller != nil)
 	end
 
-	--tie up all loose ends
+	-- tie up all loose ends
 	function SWEP:FinishCall()
 		self:SafeCallOnClient("DoFinishCall", "")
-		--terminate the call on the caller's end too
+		-- terminate the call on the caller's end too
 		if (self.Caller != nil) then
 		self:SendBuddyCallInfo(BuddyCallInfo.bciSilentCancel, self.Caller)
 		end
@@ -493,9 +495,9 @@ if SERVER then
 		self:BuddyFinderBusy(false)	
 	end
 
-	--set the buddyfinder animation, if a button was pressed then make a sound
-	--also able to specify a routine to call when done - if none specified then
-	--the swep returns to the idle animation
+	-- set the buddyfinder animation, if a button was pressed then make a sound
+	-- also able to specify a routine to call when done - if none specified then
+	-- the swep returns to the idle animation
 	function SWEP:BuddyAnim(anim, pressedButton, callWhenDone)
 		Dbg('Animating: '..tostring(anim))
 		if pressedButton then
@@ -510,47 +512,47 @@ if SERVER then
 	end
 
 	function SWEP:NPCCallOnClient(routine, params)
-		--if we need to handle client style routines for the NPC
-		--we do it here (not yet needed)
+		-- if we need to handle client style routines for the NPC
+		-- we do it here (not yet needed)
 	end
 
-	--revert buddyfinder animation to idle
+	-- revert buddyfinder animation to idle
 	function SWEP:AnimReturnToIdle()
 		self:SendWeaponAnim(BuddyFinderAnim.Idle)
 	end
 
-	--after the animation is played, the answer is sent to the callee
+	-- after the animation is played, the answer is sent to the callee
 	function SWEP:DelayedAnswer(accepted)
 		if accepted then
-		--play the new laser placement out action	 
+		-- play the new laser placement out action	 
 		self:SendBuddyCallInfo(BuddyCallInfo.bciAccepted, self.Caller)
 		self:BuddyAnim(BuddyFinderAnim.LaserBoxIn, false, function() self:StartSelectPoint() end)
 		else
-		--play the deny out action
+		-- play the deny out action
 		self:BuddyAnim(BuddyFinderAnim.DenyOut, true)
 		self:SendBuddyCallInfo(BuddyCallInfo.bciDenied, self.Caller)
 		self:FinishCall()
 		end
 	end
 
-	--called once we have decided whether to accept or deny the caller
+	-- called once we have decided whether to accept or deny the caller
 	function SWEP:AnswerBuddyFinder(accepted)
 		if accepted == "true" then
-		--play the accept action
+		-- play the accept action
 		self:BuddyAnim(BuddyFinderAnim.AcceptIn, false, function() self:DelayedAnswer(true) end)
 		else
-		--play the deny action
+		-- play the deny action
 		self:BuddyAnim(BuddyFinderAnim.DenyIn, false, function() self:DelayedAnswer(false) end)
 		end
 	end
 
-	--called when the NPC answers the buddyfinder
+	-- called when the NPC answers the buddyfinder
 	function SWEP:NPCAnswer()
 		self:DelayedAnswer(self.NPCAcceptCall)
 		self.NPCAcceptCall = not self.NPCAcceptCall --toggle	
 	end
 
-	--send a pm to another player
+	-- send a pm to another player
 	function SWEP:SendPM(toSteamId, msg)
 		local recvBuddy = GetBuddyFinder(player.GetByUniqueID(toSteamId))
 		if (recvBuddy != nil) then
@@ -558,17 +560,17 @@ if SERVER then
 		end
 	end
 
-	--a private message has been received so we print it in the
-	--chat area immediately
+	-- a private message has been received so we print it in the
+	-- chat area immediately
 	function SWEP:PrintPM(fromply, msg)
 		self:GetOwner():PrintMessage(HUD_PRINTTALK, fromply:Nick().."{PM}: "..msg)
 		self:SafeCallOnClient("MakePMSound", "")
 	end
 
-	--sends a HUD notification to the client screen
+	-- sends a HUD notification to the client screen
 	function SWEP:SendHUDNotification(start, notification)
 		if (self:GetOwner() == NULL) or (self:GetOwner():IsNPC()) then
-		return
+			return
 		end
 		umsg.Start("buddy_finder_hud_notify", self:GetOwner())
 		umsg.Bool(start)
@@ -578,45 +580,45 @@ if SERVER then
 		umsg.End()
 	end
 
-	--feedback, sent from the buddyfinder we are calling
+	-- feedback, sent from the buddyfinder we are calling
 	function SWEP:ReceiveBuddyCallInfo(callInfo, teleportVector)
 		Dbg(tostring(self:GetOwner())..' received call info '..tostring(callInfo))
-		--if at this point the ringing timer is still going, stop it
+		-- if at this point the ringing timer is still going, stop it
 		timer.Stop("BuddyRinging")
 		local hudnotify = callInfo.HUDNotify
-		--if this call info requires a hud notification then 
-		--send a message to the client to do so
+		-- if this call info requires a hud notification then 
+		-- send a message to the client to do so
 		if (hudnotify != nil) then
-		self:SendHUDNotification(true, callInfo)
+			self:SendHUDNotification(true, callInfo)
 		end
 		local nextAction = callInfo.NextAction
-		--if this call requires an action to be taken other than
-		--finishing up everything then we do it here
+		-- if this call requires an action to be taken other than
+		-- finishing up everything then we do it here
 		if (nextAction != nil) then
-		if (nextAction == "Warmup") then
-			self:DoTeleportWarmup()
-		elseif (nextAction == "Teleport") then
-			self:DoTeleport(teleportVector)
-		else
-			self:FinishCall() --the silent cancel will trigger this
-		end
+			if (nextAction == "Warmup") then
+				self:DoTeleportWarmup()
+			elseif (nextAction == "Teleport") then
+				self:DoTeleport(teleportVector)
+			else
+				self:FinishCall() --the silent cancel will trigger this
+			end
 		end
 	end
 
-	--the final piece of the puzzle, provided by the laser box
+	-- the final piece of the puzzle, provided by the laser box
 	function SWEP:SetTeleportLocation(tox, toy, toz)
 		self:SendBuddyCallInfo(BuddyCallInfo.bciTeleport, self.Caller, Vector(tox, toy, toz))
 		self.LaserBox = nil
 		self:FinishCall()
 	end
 
-	--returns true if the owner of this weapon is an NPC
+	-- returns true if the owner of this weapon is an NPC
 	function SWEP:NPCMode()
 		local theowner = self:GetOwner()
 		return (theowner != nil) and (theowner != NULL) and (theowner:IsNPC())
 	end
 
-	--a call is incoming - received here
+	-- a call is incoming - received here
 	function SWEP:ReceiveBuddyCall(	ply	)
 		Dbg('Receiving a call from '..tostring(	ply))
 		if self:BuddyFinderBusy() then
@@ -635,7 +637,7 @@ if SERVER then
 		end
 	end
 
-	--send information about the call to the caller
+	-- send information about the call to the caller
 	function SWEP:SendBuddyCallInfo(callInfo, toCaller, tableInfo)
 		local callerBuddy = GetBuddyFinder(toCaller)
 		if (callerBuddy != nil) then
@@ -646,7 +648,7 @@ if SERVER then
 		end
 	end
 
-	--after the animation, continue placing the call
+	-- after the animation, continue placing the call
 	function SWEP:DelayedMakeBuddyCall(toBuddy)
 		self:BuddyAnim(BuddyFinderAnim.DialOut, true)
 		if (toBuddy != nil) then	
@@ -657,7 +659,7 @@ if SERVER then
 		end
 	end
 
-	--start the process of calling another buddy finder
+	-- start the process of calling another buddy finder
 	function SWEP:StartBuddyCall(toPlayerSteamID)
 		if self:BuddyFinderBusy() then
 		Dbg('Cant make call - unit is busy')
@@ -685,19 +687,19 @@ if SERVER then
 		return validPlayers
 	end
 
-	--sends a usermessage with all player details
+	-- sends a usermessage with all player details
 	function SWEP:PopulatePhonePlayerList()
 		local allPlayers = table.Copy(player.GetAll())
 		local npcs = ents.FindByClass('npc_*')	
 
-		--if we are debugging then we also include 
-		--all NPCs
+		-- if we are debugging then we also include 
+		-- all NPCs
 		if bf_debug_mode then
 		table.Merge(allPlayers, npcs)
 		end
 
-		--remove players without buddy finders
-		--if not in debug mode, remove the local player and players without buddyfinders
+		-- remove players without buddy finders
+		-- if not in debug mode, remove the local player and players without buddyfinders
 		allPlayers = self:RemoveInvalidPlayers(allPlayers)
 		
 		umsg.Start("PopulatePhonePlayerList", self:GetOwner())
@@ -714,48 +716,48 @@ if SERVER then
 		umsg.End()
 	end
 
-	--after the animation, acknowledge that we have picked up the phone
+	-- after the animation, acknowledge that we have picked up the phone
 	function SWEP:DelayedPickup()
 		self:SendHUDNotification(false) --turn off the ring
 		self:SafeCallOnClient("ShowIncomingCallDialog", "")
 		self:BuddyAnim(BuddyFinderAnim.DialOut, true)
 	end
 
-	--after the animation, populate the phone list and send it via umessage
+	-- after the animation, populate the phone list and send it via umessage
 	function SWEP:DelayedContacts()
 		self:PopulatePhonePlayerList()
 		self:BuddyAnim(BuddyFinderAnim.DialOut, true)
 	end
 
-	--once the animation is played, destroy the laser box which sends out the coords
+	-- once the animation is played, destroy the laser box which sends out the coords
 	function SWEP:DoPlaceBox()
 		self:BuddyAnim(BuddyFinderAnim.LaserBoxOut, true)
 		self.LaserBox:Remove()
 	end
 
 	function SWEP:PrimaryAttack()
-		--if we are in teleport point select mode then we work differently
-		--animation pressing a button on the phone model
+		-- if we are in teleport point select mode then we work differently
+		-- animation pressing a button on the phone model
 		if (self.LaserBox != nil) then
-			--play the dial/pickup action
+			-- play the dial/pickup action
 			Dbg('Attack: Teleport')
 			self:DoPlaceBox()
 		elseif self:HasIncomingCall() then
-			--play the dial/pickup action
+			-- play the dial/pickup action
 			Dbg('Attack: Answer')
 			self:BuddyAnim(BuddyFinderAnim.DialIn, false, function() self:DelayedPickup() end)
 		else
-			--play the dial/pickup action
+			-- play the dial/pickup action
 			Dbg('Attack: Dial')
 			self:BuddyAnim(BuddyFinderAnim.DialIn, false, function() self:DelayedContacts() end) 
 		end
 	end
 
-	--tidy up
+	-- tidy up
 	function SWEP:OnRemove()
 		self:SendHUDNotification(false)
 		self:FinishCall() 
-		--should fix the aimpos spam
+		-- should fix the aimpos spam
 		if (self.LaserBox != nil) then
 			self.LaserBox:Remove()
 		end
